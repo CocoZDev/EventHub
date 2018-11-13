bba2620d826e34e1a13a782812fcaa022399b0af
Zoey007 <msguiyuzhao@gmail.com>
2018-11-12 20:35:42 -0800

    added scroll to top button and adjusted style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,11 @@ function dayAndNight(){
 
     if (day_night > 0 && day_night < 17){
       //Day
-      document.body.style.backgroundImage = "url('assets/images/sunrise.jpg')";
+      document.body.style.backgroundImage = "url('assets/images/sunrise_org.jpg')";
     }
     else{
       //Night
-      document.body.style.backgroundImage = "url('assets/images/night.jpg')";
+      document.body.style.backgroundImage = "url('assets/images/night_org.jpg')";
     }
 }
 dayAndNight();//======background change end code
@@ -38,17 +38,17 @@ $("#submit-id").click(function(){
   $("#event-type-container").removeClass("hidden");
 });
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Cancel the default action, if needed
-  event.preventDefault();
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    console.log('input Captured: ' + input);
-    document.getElementById("submit-id").click();
-  }
-});
+// // Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function(event) {
+//   // Cancel the default action, if needed
+//   event.preventDefault();
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Trigger the button element with a click
+//     console.log('input Captured: ' + input);
+//     document.getElementById("submit-id").click();
+//   }
+// });
 
 //using weather api key getting weather details
   function getWeather(){
@@ -111,3 +111,24 @@ input.addEventListener("keyup", function(event) {
       updateClock(clockElement);
   }, 1000);
 }());
+
+// ========== Smooth Scroll To Top Button - https://codepen.io/kruxor/pen/CwpFq ========== //
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $('.scrolltop:hidden')
+      .stop(true, true)
+      .fadeIn();
+  } else {
+    $('.scrolltop')
+      .stop(true, true)
+      .fadeOut();
+  }
+});
+
+$(function() {
+  $('.scroll-btn').click(function() {
+    $('html,body').animate({ scrollTop: $('#page-top-anchor').offset().top - 50}, '500');
+    return false;
+  });
+});
